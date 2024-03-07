eda7ed245c6123980cec9080b496b5b388f50155
Matheus Alexandre <matheuscurses@gmail.com>
2024-03-07 15:57:09 -0300

    Criando a tela de Criar Contra utilizando a arquitetura do ASP.NET MVC, e no form foi acrescentado 3 coisas
    asp-controller="Account": Define o controlador do ASP.NET MVC que será responsável por lidar com a submissão do formulário. Neste caso, quando o formulário for enviado, ele será enviado para o controlador chamado "Account".
    
    asp-action="Register": Define a ação do controlador que será chamada quando o formulário for enviado. Neste caso, quando o formulário for enviado, ele chamará a ação "Register" no controlador "Account".
    
    method="post": Define o método HTTP que será usado para enviar o formulário. Neste caso, o método "post" será usado, o que significa que os dados do formulário serão enviados de forma não visível ao usuário (via corpo da requisição) e é adequado para o envio de dados sensíveis, como informações de login ou cadastro.

---
+++ ironbody.academias.Application/Controllers/Account/Account.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ironbody.academias.Application.Controllers.Account;
+public class Account : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    public IActionResult Register()
+    {
+        return View();
+    }
+   
+
+    public IActionResult Login()
+    {
+        return View();
+    }
+}
---
+++ ironbody.academias.Application/Models/Account/UserViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ironbody.academias.Application.Models.Account;
+
+public class UserViewModel
+{
+    [Display(Name = "Primeiro nome")]
+    public string? FirstName { get; set; }
+
+    [Display(Name = "Ultimo nome")]
+    public string? LastName { get; set; }
+
+    public string? Email { get; set; }
+
+    [Display(Name = "Nome de usuário")]
+    public string? UserName { get; set; }
+
+    [Display(Name = "Senha")]
+    public string? Password { get; set; }
+
+    [Display(Name = "Confirm a senha")]
+    public string? ConfirmPassword { get; set; }
+}
